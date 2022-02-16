15bc41c0fb7e8c92a59d4f6377a1441b50585932
tinokingstone <tino.kingstone@digital.cabinet-office.gov.uk>
2022-02-16 14:36:16 +0000

    wip: session id is stored into dynamo db table

--- lambdas/session/src/main/java/uk/gov/di/ipv/cri/address/api/handler/SessionHandler.java
+++ lambdas/session/src/main/java/uk/gov/di/ipv/cri/address/api/handler/SessionHandler.java
@@ -1,27 +1,45 @@
 package uk.gov.di.ipv.cri.address.api.handler;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
-import org.apache.http.HttpStatus;
-
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.UUID;
+import org.apache.http.HttpStatus;
 
 public class SessionHandler
-        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+  implements
+    RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    @Override
-    public APIGatewayProxyResponseEvent handleRequest(
-            APIGatewayProxyRequestEvent input, Context context) {
-        APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent =
-                new APIGatewayProxyResponseEvent();
-        apiGatewayProxyResponseEvent.setStatusCode(HttpStatus.SC_CREATED);
-        UUID uuid = UUID.randomUUID();
-        Map<String, String> responseMap = Map.of("session_id", uuid.toString());
-        apiGatewayProxyResponseEvent.setBody(new Gson().toJson(responseMap));
-        return apiGatewayProxyResponseEvent;
-    }
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(
+    APIGatewayProxyRequestEvent input,
+    Context context
+  ) {
+    APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent = new APIGatewayProxyResponseEvent();
+    apiGatewayProxyResponseEvent.setStatusCode(HttpStatus.SC_CREATED);
+    UUID uuid = UUID.randomUUID();
+    // write to dynamoDB
+    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    DynamoDB dynamoDB = new DynamoDB(client);
+    String tableName = "address-session-tino-cri-address-api"; // use env variable
+    Table table = dynamoDB.getTable(tableName);
+    Item item = new Item().withPrimaryKey("session-id", uuid.toString());
+    table.putItem(item);
+
+    Map<String, String> responseMap = Map.of("session_id", uuid.toString());
+    apiGatewayProxyResponseEvent.setBody(new Gson().toJson(responseMap));
+    return apiGatewayProxyResponseEvent;
+  }
 }
