cfd7575f9db0bf97769fa15dedd2928abb9b37bd
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2022-02-21 14:56:14 +0000

    Add lib module as a dependency used by lambdas

---
+++ lib/src/main/java/uk/gov/di/ipv/cri/fraud/library/exception/AccessTokenProcessingException.java
@@ -0,0 +1,7 @@
+package uk.gov.di.ipv.cri.fraud.library.exception;
+
+public class AccessTokenProcessingException extends Exception {
+    public AccessTokenProcessingException() {
+        super();
+    }
+}
---
+++ lib/src/main/java/uk/gov/di/ipv/cri/fraud/library/validation/ValidationResult.java
@@ -0,0 +1,23 @@
+package uk.gov.di.ipv.cri.fraud.library.validation;
+
+public class ValidationResult<T> {
+    private final boolean valid;
+    private final T error;
+
+    public ValidationResult(boolean valid, T error) {
+        this.valid = valid;
+        this.error = error;
+    }
+
+    public static <U> ValidationResult<U> createValidResult() {
+        return new ValidationResult<>(true, null);
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public T getError() {
+        return error;
+    }
+}
