897b8d3ed613ae6c5f238bef342595296ed38b0c
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2022-02-25 22:25:21 +0000

    Session lambda handler

--- lambdas/session/src/main/java/uk/gov/di/ipv/cri/address/api/handler/SessionHandler.java
+++ lambdas/session/src/main/java/uk/gov/di/ipv/cri/address/api/handler/SessionHandler.java
@@ -6,21 +6,37 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import org.apache.http.HttpStatus;
+import uk.gov.di.ipv.cri.address.library.annotations.ExcludeFromGeneratedCoverageReport;
+import uk.gov.di.ipv.cri.address.library.service.AddressSessionService;
 
 import java.util.Map;
-import java.util.UUID;
 
 public class SessionHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    protected static final String SESSION_ID = "session_id";
+    private AddressSessionService addressSessionService;
+
+    public SessionHandler() {
+        addressSessionService = new AddressSessionService();
+    }
+
+    @ExcludeFromGeneratedCoverageReport
+    public SessionHandler(AddressSessionService addressSessionService) {
+        this.addressSessionService = addressSessionService;
+    }
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent input, Context context) {
+
+        // todo validate request params and claims JWT
+
+        String sessionId = addressSessionService.createAndSaveAddressSession();
+
         APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent =
                 new APIGatewayProxyResponseEvent();
         apiGatewayProxyResponseEvent.setStatusCode(HttpStatus.SC_CREATED);
-        UUID uuid = UUID.randomUUID();
-        Map<String, String> responseMap = Map.of("session_id", uuid.toString());
+        Map<String, String> responseMap = Map.of(SESSION_ID, sessionId);
         apiGatewayProxyResponseEvent.setBody(new Gson().toJson(responseMap));
         return apiGatewayProxyResponseEvent;
     }
--- lambdas/session/src/test/java/uk/gov/di/ipv/cri/address/api/handler/SessionHandlerTest.java
+++ lambdas/session/src/test/java/uk/gov/di/ipv/cri/address/api/handler/SessionHandlerTest.java
@@ -1,4 +1,48 @@
-import static org.junit.jupiter.api.Assertions.*;
+package uk.gov.di.ipv.cri.address.api.handler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.ipv.cri.address.library.service.AddressSessionService;
+
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static uk.gov.di.ipv.cri.address.api.handler.SessionHandler.SESSION_ID;
+
+@ExtendWith(MockitoExtension.class)
 class SessionHandlerTest {
-  
+
+    @Mock private AddressSessionService addressSessionService;
+
+    @Mock private APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent;
+
+    private SessionHandler sessionHandler;
+
+    @BeforeEach
+    void setUp() {
+        sessionHandler = new SessionHandler(addressSessionService);
+    }
+
+    @Test
+    void shouldCreateAndSaveAddressSession() throws JsonProcessingException {
+        String sessionId = UUID.randomUUID().toString();
+        when(addressSessionService.createAndSaveAddressSession()).thenReturn(sessionId);
+        APIGatewayProxyResponseEvent responseEvent =
+                sessionHandler.handleRequest(apiGatewayProxyRequestEvent, null);
+        assertEquals(HttpStatus.SC_CREATED, responseEvent.getStatusCode());
+        Map responseBody = new ObjectMapper().readValue(responseEvent.getBody(), Map.class);
+        assertEquals(sessionId, responseBody.get(SESSION_ID));
+        verifyNoInteractions(apiGatewayProxyRequestEvent);
+    }
 }
