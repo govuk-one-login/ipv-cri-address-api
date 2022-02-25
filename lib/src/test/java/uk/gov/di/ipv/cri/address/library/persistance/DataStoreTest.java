4e306e5be3a54d9fd6d22861ed518512ba017cd7
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2022-02-25 22:37:10 +0000

    Add DataStoreTest

--- lib/src/test/java/uk/gov/di/ipv/cri/address/library/persistance/DataStoreTest.java
+++ lib/src/test/java/uk/gov/di/ipv/cri/address/library/persistance/DataStoreTest.java
@@ -1,12 +1,25 @@
 package uk.gov.di.ipv.cri.address.library.persistance;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import uk.gov.di.ipv.cri.address.library.persistence.DataStore;
 import uk.gov.di.ipv.cri.address.library.persistence.item.AddressSessionItem;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DataStoreTest {
@@ -15,103 +28,37 @@ class DataStoreTest {
     @Mock private DynamoDbEnhancedClient mockDynamoDbEnhancedClient;
     @Mock private DynamoDbTable<AddressSessionItem> mockDynamoDbTable;
 
-    private AddressSessionItem accessTokenItem;
+    private AddressSessionItem addressSessionItem;
     private DataStore<AddressSessionItem> dataStore;
 
-    //    @BeforeEach
-    //    void setUp() {
-    //        when(mockDynamoDbEnhancedClient.table(
-    //                        anyString(), ArgumentMatchers.<TableSchema<AddressSessionItem>>any()))
-    //                .thenReturn(mockDynamoDbTable);
-    //
-    //        accessTokenItem = new AddressSessionItem();
-    //        String accessToken = UUID.randomUUID().toString();
-    //        accessTokenItem.setAccessToken(accessToken);
-    //        accessTokenItem.setResourceId("test-resource-12345");
-    //
-    //        dataStore =
-    //                new DataStore<>(
-    //                        TEST_TABLE_NAME, AddressSessionItem.class,
-    // mockDynamoDbEnhancedClient);
-    //    }
+    @BeforeEach
+    void setUp() {
+        when(mockDynamoDbEnhancedClient.table(
+                        anyString(), ArgumentMatchers.<TableSchema<AddressSessionItem>>any()))
+                .thenReturn(mockDynamoDbTable);
 
-    //    @Test
-    //    void shouldPutItemIntoDynamoDbTable() {
-    //        dataStore.create(accessTokenItem);
-    //
-    //        ArgumentCaptor<AddressSessionItem> authorizationCodeItemArgumentCaptor =
-    //                ArgumentCaptor.forClass(AddressSessionItem.class);
-    //
-    //        verify(mockDynamoDbEnhancedClient)
-    //                .table(
-    //                        eq(TEST_TABLE_NAME),
-    //                        ArgumentMatchers.<TableSchema<AddressSessionItem>>any());
-    //        verify(mockDynamoDbTable).putItem(authorizationCodeItemArgumentCaptor.capture());
-    //        assertEquals(
-    //                accessTokenItem.getAccessToken(),
-    //                authorizationCodeItemArgumentCaptor.getValue().getAccessToken());
-    //        assertEquals(
-    //                accessTokenItem.getResourceId(),
-    //                authorizationCodeItemArgumentCaptor.getValue().getResourceId());
-    //    }
+        addressSessionItem = new AddressSessionItem();
+        String accessToken = UUID.randomUUID().toString();
 
-    //    @Test
-    //    void shouldGetItemFromDynamoDbTableViaPartitionKeyAndSortKey() {
-    //        dataStore.getItem("partition-key-12345", "sort-key-12345");
-    //
-    //        ArgumentCaptor<Key> keyCaptor = ArgumentCaptor.forClass(Key.class);
-    //
-    //        verify(mockDynamoDbEnhancedClient)
-    //                .table(
-    //                        eq(TEST_TABLE_NAME),
-    //                        ArgumentMatchers.<TableSchema<AddressSessionItem>>any());
-    //        verify(mockDynamoDbTable).getItem(keyCaptor.capture());
-    //        assertEquals("partition-key-12345", keyCaptor.getValue().partitionKeyValue().s());
-    //        assertEquals("sort-key-12345", keyCaptor.getValue().sortKeyValue().get().s());
-    //    }
+        dataStore =
+                new DataStore<>(
+                        TEST_TABLE_NAME, AddressSessionItem.class, mockDynamoDbEnhancedClient);
+    }
 
-    //    @Test
-    //    void shouldGetItemFromDynamoDbTableViaPartitionKey() {
-    //        dataStore.getItem("partition-key-12345");
-    //
-    //        ArgumentCaptor<Key> keyCaptor = ArgumentCaptor.forClass(Key.class);
-    //
-    //        verify(mockDynamoDbEnhancedClient)
-    //                .table(
-    //                        eq(TEST_TABLE_NAME),
-    //                        ArgumentMatchers.<TableSchema<AddressSessionItem>>any());
-    //        verify(mockDynamoDbTable).getItem(keyCaptor.capture());
-    //        assertEquals("partition-key-12345", keyCaptor.getValue().partitionKeyValue().s());
-    //        assertTrue(keyCaptor.getValue().sortKeyValue().isEmpty());
-    //    }
+    @Test
+    void shouldPutItemIntoDynamoDbTable() {
+        dataStore.create(addressSessionItem);
 
-    //    @Test
-    //    void shouldDeleteItemFromDynamoDbTableViaPartitionKeyAndSortKey() {
-    //        dataStore.delete("partition-key-12345", "sort-key-12345");
-    //
-    //        ArgumentCaptor<Key> keyCaptor = ArgumentCaptor.forClass(Key.class);
-    //
-    //        verify(mockDynamoDbEnhancedClient)
-    //                .table(
-    //                        eq(TEST_TABLE_NAME),
-    //                        ArgumentMatchers.<TableSchema<AddressSessionItem>>any());
-    //        verify(mockDynamoDbTable).deleteItem(keyCaptor.capture());
-    //        assertEquals("partition-key-12345", keyCaptor.getValue().partitionKeyValue().s());
-    //        assertEquals("sort-key-12345", keyCaptor.getValue().sortKeyValue().get().s());
-    //    }
+        ArgumentCaptor<AddressSessionItem> authorizationCodeItemArgumentCaptor =
+                ArgumentCaptor.forClass(AddressSessionItem.class);
 
-    //    @Test
-    //    void shouldDeleteItemFromDynamoDbTableViaPartitionKey() {
-    //        dataStore.delete("partition-key-12345");
-    //
-    //        ArgumentCaptor<Key> keyCaptor = ArgumentCaptor.forClass(Key.class);
-    //
-    //        verify(mockDynamoDbEnhancedClient)
-    //                .table(
-    //                        eq(TEST_TABLE_NAME),
-    //                        ArgumentMatchers.<TableSchema<AddressSessionItem>>any());
-    //        verify(mockDynamoDbTable).deleteItem(keyCaptor.capture());
-    //        assertEquals("partition-key-12345", keyCaptor.getValue().partitionKeyValue().s());
-    //        assertTrue(keyCaptor.getValue().sortKeyValue().isEmpty());
-    //    }
+        verify(mockDynamoDbEnhancedClient)
+                .table(
+                        eq(TEST_TABLE_NAME),
+                        ArgumentMatchers.<TableSchema<AddressSessionItem>>any());
+        verify(mockDynamoDbTable).putItem(authorizationCodeItemArgumentCaptor.capture());
+        assertEquals(
+                addressSessionItem.getSessionId(),
+                authorizationCodeItemArgumentCaptor.getValue().getSessionId());
+    }
 }
