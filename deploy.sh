68ec3a8ae06aacedbf2d7a917d331c2209ae01a2
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2022-03-14 14:54:29 +0000

    Set a global CodeSigningConfigArn for code signing config

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -eu
 ./gradlew
 sam build -t deploy/template.yaml --config-env dev
 sam validate -t deploy/template.yaml --config-env dev
-sam deploy -t deploy/template.yaml --config-env dev --config-file samconfig.toml --no-fail-on-empty-changeset
+sam deploy -t deploy/template.yaml --config-env dev --config-file samconfig.toml --no-fail-on-empty-changeset --signing-profiles SessionFunction=DiIpvCriSigningProfile PostcodeLookupFunction=DiIpvCriSigningProfile
