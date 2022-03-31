c8acc412c4ca9b32e9a099ec6272cfa8b1b6a329
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2022-03-31 18:32:14 +0100

    KBV-156: Wire the EventProbe

--- lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/address/api/handler/IssueCredentialHandler.java
+++ lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/address/api/handler/IssueCredentialHandler.java
@@ -5,26 +5,30 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.Level;
 import software.amazon.lambda.powertools.logging.CorrelationIdPathConstants;
 import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.metrics.Metrics;
 import uk.gov.di.ipv.cri.address.library.exception.CredentialRequestException;
 import uk.gov.di.ipv.cri.address.library.helpers.ApiGatewayResponseGenerator;
+import uk.gov.di.ipv.cri.address.library.helpers.EventProbe;
 import uk.gov.di.ipv.cri.address.library.service.CredentialIssuerService;
-
-import java.util.UUID;
 
 public class IssueCredentialHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private EventProbe eventProbe;
     private final CredentialIssuerService addressCredentialIssuerService;
 
-    public IssueCredentialHandler(CredentialIssuerService addressCredentialIssuerService) {
+    public IssueCredentialHandler(
+            CredentialIssuerService addressCredentialIssuerService, EventProbe eventProbe) {
         this.addressCredentialIssuerService = addressCredentialIssuerService;
+        this.eventProbe = eventProbe;
     }
 
     public IssueCredentialHandler() {
         this.addressCredentialIssuerService = new CredentialIssuerService();
+        this.eventProbe = new EventProbe();
     }
 
     @Override
@@ -35,10 +39,12 @@ public class IssueCredentialHandler
 
         APIGatewayProxyResponseEvent response;
         try {
-            UUID sessionId = addressCredentialIssuerService.getSessionId(input);
+            var sessionId = addressCredentialIssuerService.getSessionId(input);
             var addresses = addressCredentialIssuerService.getAddresses(sessionId);
+            eventProbe.counterMetric("credential");
             response = ApiGatewayResponseGenerator.proxyJsonResponse(HttpStatus.SC_OK, 1);
         } catch (CredentialRequestException e) {
+            eventProbe.log(Level.ERROR, e).counterMetric("credential", 0d);
             response = ApiGatewayResponseGenerator.proxyJsonResponse(HttpStatus.SC_BAD_REQUEST, 0);
         }
         return response;
--- lambdas/issuecredential/src/test/java/uk/gov/di/ipv/cri/address/api/handler/IssueCredentialHandlerTest.java
+++ lambdas/issuecredential/src/test/java/uk/gov/di/ipv/cri/address/api/handler/IssueCredentialHandlerTest.java
@@ -5,18 +5,23 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.nimbusds.common.contenttype.ContentType;
 import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.Level;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.di.ipv.cri.address.library.exception.CredentialRequestException;
+import uk.gov.di.ipv.cri.address.library.helpers.EventProbe;
 import uk.gov.di.ipv.cri.address.library.service.CredentialIssuerService;
 
 import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,11 +29,12 @@ import static org.mockito.Mockito.when;
 class IssueCredentialHandlerTest {
     @Mock private Context context;
     @Mock private CredentialIssuerService mockAddressCredentialIssuerService;
+    @Mock private EventProbe eventProbe;
     private IssueCredentialHandler handler;
 
     @BeforeEach
     void setUp() {
-        handler = new IssueCredentialHandler(mockAddressCredentialIssuerService);
+        handler = new IssueCredentialHandler(mockAddressCredentialIssuerService, eventProbe);
     }
 
     @Test
@@ -47,12 +53,14 @@ class IssueCredentialHandlerTest {
 
         assertEquals(HttpStatus.SC_OK, response.getStatusCode());
         assertEquals(response.getBody(), "1");
+        verify(eventProbe).counterMetric("credential");
     }
 
     @Test
     void shouldThrowCredentialRequestExceptionWhenSubjectIsNotSupplied()
             throws CredentialRequestException {
         APIGatewayProxyRequestEvent event = new APIGatewayProxyRequestEvent();
+        setupEventProbeErrorBehaviour();
         event.withHeaders(
                 Map.of(CredentialIssuerService.AUTHORIZATION, UUID.randomUUID().toString()));
 
@@ -66,6 +74,7 @@ class IssueCredentialHandlerTest {
 
         assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusCode());
         assertEquals(response.getBody(), "0");
+        verify(eventProbe).counterMetric("credential", 0d);
     }
 
     @Test
@@ -73,6 +82,7 @@ class IssueCredentialHandlerTest {
             throws CredentialRequestException {
         APIGatewayProxyRequestEvent event = new APIGatewayProxyRequestEvent();
         event.withBody("sub=subject");
+        setupEventProbeErrorBehaviour();
 
         when(mockAddressCredentialIssuerService.getSessionId(event))
                 .thenThrow(CredentialRequestException.class);
@@ -84,5 +94,11 @@ class IssueCredentialHandlerTest {
 
         assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusCode());
         assertEquals(response.getBody(), "0");
+        verify(eventProbe).counterMetric("credential", 0d);
+    }
+
+    private void setupEventProbeErrorBehaviour() {
+        when(eventProbe.counterMetric(anyString(), anyDouble())).thenReturn(eventProbe);
+        when(eventProbe.log(any(Level.class), any(Exception.class))).thenReturn(eventProbe);
     }
 }
