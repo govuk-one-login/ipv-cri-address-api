75e72ec36130123e6271352ec4bb754b2791b954
Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>
2022-05-04 10:24:32 +0100

    KBV-324: added unit test for Audit Service

---
+++ lib/src/test/java/uk/gov/di/ipv/cri/address/library/service/AuditServiceTest.java
@@ -0,0 +1,61 @@
+package uk.gov.di.ipv.cri.address.library.service;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.ipv.cri.address.library.domain.AuditEventTypes;
+import uk.gov.di.ipv.cri.address.library.exception.SqsException;
+import uk.gov.di.ipv.cri.address.library.models.AuditEvent;
+
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AuditServiceTest {
+    @Mock AmazonSQS mockSqs;
+    @Mock ConfigurationService mockConfigurationService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private AuditService auditService;
+
+    @BeforeEach
+    void setup() {
+        when(mockConfigurationService.getSqsAuditEventQueueUrl())
+                .thenReturn("https://example-queue-url");
+
+        auditService = new AuditService(mockSqs, mockConfigurationService);
+    }
+
+    @Test
+    void shouldSendMessageToSqsQueue() throws JsonProcessingException, SqsException {
+        UUID sessionId = UUID.randomUUID();
+        String clientId = "client-id";
+        auditService.sendAuditEvent(AuditEventTypes.SESSION_CREATED, sessionId, clientId);
+
+        ArgumentCaptor<SendMessageRequest> sqsSendMessageRequestCaptor =
+                ArgumentCaptor.forClass(SendMessageRequest.class);
+        verify(mockSqs).sendMessage(sqsSendMessageRequestCaptor.capture());
+
+        assertEquals(
+                "https://example-queue-url", sqsSendMessageRequestCaptor.getValue().getQueueUrl());
+
+        AuditEvent messageBody =
+                objectMapper.readValue(
+                        sqsSendMessageRequestCaptor.getValue().getMessageBody(), AuditEvent.class);
+        assertEquals(AuditEventTypes.SESSION_CREATED, messageBody.getEvent());
+        assertEquals(clientId, messageBody.getClientId());
+        assertEquals(sessionId.toString(), messageBody.getEventId());
+    }
+}
