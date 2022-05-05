6fde6f4b94658b459e423651dc36c5a2bceaeed2
Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>
2022-05-05 11:38:03 +0100

    KBV-324: addressed PR comments

--- lib/src/test/java/uk/gov/di/ipv/cri/address/library/service/AuditServiceTest.java
+++ lib/src/test/java/uk/gov/di/ipv/cri/address/library/service/AuditServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.di.ipv.cri.address.library.domain.AuditEvent;
