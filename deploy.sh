5c871074cfc72822ac02cefc07ca70a3753d93b4
Matthew Bidewell <matthewbidewell@gmail.com>
2022-09-16 15:01:40 +0100

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-./gradlew
+# ./gradlew
 sam validate -t infrastructure/lambda/template.yaml
 sam build -t infrastructure/lambda/template.yaml --config-env dev
 sam deploy -t infrastructure/lambda/template.yaml --config-env dev --config-file samconfig.toml --no-fail-on-empty-changeset
