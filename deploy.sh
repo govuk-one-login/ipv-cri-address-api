1bf7c870d497d015895e02e78307db7329df3d16
Matthew Bidewell <matthewbidewell@gmail.com>
2022-09-20 21:27:07 +0100

    Fix on deployment script for NodeJS

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-# ./gradlew
+
 sam validate -t infrastructure/lambda/template.yaml
-sam build -t infrastructure/lambda/template.yaml --config-env dev
-sam deploy -t infrastructure/lambda/template.yaml --config-env dev --config-file samconfig.toml --no-fail-on-empty-changeset
+sam build -t infrastructure/lambda/template.yaml
+sam deploy --config-env dev --config-file infrastructure/lambda/samconfig.toml --no-fail-on-empty-changeset
