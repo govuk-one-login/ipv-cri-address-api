8b5422bb7c7cf6506a1f36b1de56cd788ef177f5
gokhakan <hakangok@icloud.com>
2022-09-26 12:30:06 +0100

    OJ-966 WIP first test step implementation

--- integration-tests/src/test/java/gov/uk/address/api/stepDefinitions/AddressApiHappyPath.java
+++ integration-tests/src/test/java/gov/uk/address/api/stepDefinitions/AddressApiHappyPath.java
@@ -1,11 +1,103 @@
 package gov.uk.address.api.stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
+import java.util.Optional;
 
 public class AddressApiHappyPath {
+    private String userIdentityJson;
+    public final String ADDRESS_CRI_DEV = "address-cri-build";
+    private String sessionRequestBody;
+
+
     @Given("user has the user identity in the form of a signed JWT string")
-    public void user_has_the_user_identity_in_the_form_of_a_signed_jwt_string() {
-        System.out.println("This is our Given");
+    public void userHasTheUserIdentityInTheFormOfASignedJWTString()
+            throws URISyntaxException, IOException, InterruptedException {
+        int experianRowNumber = 681;
+        userIdentityJson = getClaimsForUser(getIPVCoreStubURL(), experianRowNumber);
+        sessionRequestBody = createRequest(getIPVCoreStubURL(), userIdentityJson);
+        System.out.println("sessionRequestBody = " + sessionRequestBody);
     }
+
+    private String getClaimsForUser(String baseUrl, int userDataRowNumber)
+            throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(
+                                new URIBuilder(baseUrl)
+                                        .setPath("backend/generateInitialClaimsSet")
+                                        .addParameter("cri", ADDRESS_CRI_DEV)
+                                        .addParameter(
+                                                "rowNumber", String.valueOf(userDataRowNumber))
+                                        .build())
+                        .GET()
+                        .build();
+        return sendHttpRequest(request).body();
+    }
+
+    private HttpResponse<String> sendHttpRequest(HttpRequest request)
+            throws IOException, InterruptedException {
+
+        String basicAuthUser =
+                Objects.requireNonNull(
+                        System.getenv("IPV_CORE_STUB_BASIC_AUTH_USER"),
+                        "Environment variable IPV_CORE_STUB_BASIC_AUTH_USER is not set");
+        String basicAuthPassword =
+                Objects.requireNonNull(
+                        System.getenv("IPV_CORE_STUB_BASIC_AUTH_PASSWORD"),
+                        "Environment variable IPV_CORE_STUB_BASIC_AUTH_PASSWORD is not set");
+
+        HttpClient client =
+                HttpClient.newBuilder()
+                        .authenticator(
+                                new Authenticator() {
+                                    @Override
+                                    protected PasswordAuthentication getPasswordAuthentication() {
+                                        return new PasswordAuthentication(
+                                                basicAuthUser, basicAuthPassword.toCharArray());
+                                    }
+                                })
+                        .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private String getIPVCoreStubURL() {
+        return Optional.ofNullable(System.getenv("IPV_CORE_STUB_URL"))
+                .orElseThrow(
+                        () ->
+                                new IllegalArgumentException(
+                                        "Environment variable IPV_CORE_STUB_URL is not set"));
+    }
+
+    private String createRequest(String baseUrl, String jsonString)
+            throws URISyntaxException, IOException, InterruptedException {
+
+        var uri =
+                new URIBuilder(baseUrl)
+                        .setPath("backend/createSessionRequest")
+                        .addParameter("cri", ADDRESS_CRI_DEV)
+                        .build();
+
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(uri)
+                        .setHeader("Accept", "application/json")
+                        .setHeader("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                        .build();
+
+        return sendHttpRequest(request).body();
+    }
+
 
 }
