cfbd63336b85be9791ec9144f8b06eddeecedb67
Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>
2022-10-05 14:07:40 +0100

    feat: wip on ts addresses lambda

---
+++ lambdas/addresses/app.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response: APIGatewayProxyResult;
+
+    // get session from session id
+
+    // get user data from session
+
+    // pull out addresses, if any, from user data
+
+    // return [] of address data or [] on a 200
+
+
+    try {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'hello world',
+            }),
+        };
+    } catch (err: unknown) {
+        console.log(err);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err instanceof Error ? err.message : 'some error happened',
+            }),
+        };
+    }
+
+    return response;
+};
