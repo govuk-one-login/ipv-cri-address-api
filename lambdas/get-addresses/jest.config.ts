c8a035dca15ce51566f072df6f37590a4022b2cb
Matthew Bidewell <matthewbidewell@gmail.com>
2022-10-07 09:02:17 +0100

    linted and added lint config

--- lambdas/get-addresses/jest.config.ts
+++ lambdas/get-addresses/jest.config.ts
@@ -5,11 +5,11 @@
 
 export default {
     transform: {
-        '^.+\\.ts?$': 'esbuild-jest',
+        "^.+\\.ts?$": "esbuild-jest",
     },
     clearMocks: true,
     collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    testMatch: ['**/tests/unit/*.test.ts'],
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    testMatch: ["**/tests/unit/*.test.ts"],
 };
--- lambdas/get-addresses/tests/unit/test-handler.test.ts
+++ lambdas/get-addresses/tests/unit/test-handler.test.ts
@@ -1,55 +1,55 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../app';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { lambdaHandler } from "../../src/app";
 
-describe('Unit test for app handler', function () {
-    it('verifies successful response', async () => {
+describe("Unit test for app handler", function () {
+    it("verifies successful response", async () => {
         const event: APIGatewayProxyEvent = {
-            httpMethod: 'get',
-            body: '',
+            httpMethod: "get",
+            body: "",
             headers: {},
             isBase64Encoded: false,
             multiValueHeaders: {},
             multiValueQueryStringParameters: {},
-            path: '/hello',
+            path: "/hello",
             pathParameters: {},
             queryStringParameters: {},
             requestContext: {
-                accountId: '123456789012',
-                apiId: '1234',
+                accountId: "123456789012",
+                apiId: "1234",
                 authorizer: {},
-                httpMethod: 'get',
+                httpMethod: "get",
                 identity: {
-                    accessKey: '',
-                    accountId: '',
-                    apiKey: '',
-                    apiKeyId: '',
-                    caller: '',
+                    accessKey: "",
+                    accountId: "",
+                    apiKey: "",
+                    apiKeyId: "",
+                    caller: "",
                     clientCert: {
-                        clientCertPem: '',
-                        issuerDN: '',
-                        serialNumber: '',
-                        subjectDN: '',
-                        validity: { notAfter: '', notBefore: '' },
+                        clientCertPem: "",
+                        issuerDN: "",
+                        serialNumber: "",
+                        subjectDN: "",
+                        validity: { notAfter: "", notBefore: "" },
                     },
-                    cognitoAuthenticationProvider: '',
-                    cognitoAuthenticationType: '',
-                    cognitoIdentityId: '',
-                    cognitoIdentityPoolId: '',
-                    principalOrgId: '',
-                    sourceIp: '',
-                    user: '',
-                    userAgent: '',
-                    userArn: '',
+                    cognitoAuthenticationProvider: "",
+                    cognitoAuthenticationType: "",
+                    cognitoIdentityId: "",
+                    cognitoIdentityPoolId: "",
+                    principalOrgId: "",
+                    sourceIp: "",
+                    user: "",
+                    userAgent: "",
+                    userArn: "",
                 },
-                path: '/hello',
-                protocol: 'HTTP/1.1',
-                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+                path: "/hello",
+                protocol: "HTTP/1.1",
+                requestId: "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
                 requestTimeEpoch: 1428582896000,
-                resourceId: '123456',
-                resourcePath: '/hello',
-                stage: 'dev',
+                resourceId: "123456",
+                resourcePath: "/hello",
+                stage: "dev",
             },
-            resource: '',
+            resource: "",
             stageVariables: {},
         };
         const result: APIGatewayProxyResult = await lambdaHandler(event);
@@ -57,7 +57,7 @@ describe('Unit test for app handler', function () {
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(
             JSON.stringify({
-                message: 'hello world',
+                message: "hello world",
             }),
         );
     });
