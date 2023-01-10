5d548e09a02227173164f4b7be1e92f7a10c2f1d
gokhakan <hakangok@icloud.com>
2023-01-10 13:32:09 +0000

    OJ-1097 add Allure reporting

--- integration-tests/src/test/java/gov/uk/address/api/runners/RunCucumberTest.java
+++ integration-tests/src/test/java/gov/uk/address/api/runners/RunCucumberTest.java
@@ -6,8 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json", "html:target/default-html-reports"},
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
         features = "src/test/resources/features",
         glue = "gov/uk/address/api/stepdefinitions",
         dryRun = false)
-public class RunCucumberTest {}
+public class RunCucumberTest {
+}
