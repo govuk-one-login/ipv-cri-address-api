206a9d64242e062752ad7f20c88f823c7c78bf93
Karol C. Gancarz <karol.sinx@gmail.com>
2023-12-13 21:52:47 +0000

    OJ-2209: Update TypeScript config from the template repo

--- lambdas/get-addresses/.eslintrc.js
+++ lambdas/get-addresses/.eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
+    root: true,
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.eslint.json", "./tsconfig.json"],
         sourceType: "module",
+        ecmaVersion: 2022,
+        ecmaFeatures: {
+            impliedStrict: true,
+        },
     },
     env: {
         node: true,
-        es2020: true,
+        es2022: true,
         jest: true,
     },
     globals: {
         sinon: true,
         expect: true,
     },
-    root: true,
-    extends: ["eslint:recommended", "prettier", "plugin:@typescript-eslint/recommended"],
+    plugins: ["@typescript-eslint"],
+    extends: ["prettier", "eslint:recommended", "plugin:prettier/recommended", "plugin:@typescript-eslint/recommended"],
+    ignorePatterns: ["node_modules", ".aws-sam", "build", "dist", "dotenv", "coverage"],
     rules: {
         "no-console": 2,
         "padding-line-between-statements": ["error", { blankLine: "any", prev: "*", next: "*" }],
