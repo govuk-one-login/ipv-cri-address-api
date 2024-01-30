635884906fbe23f9619681d02fefe825879d51a5
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-01-30 14:46:56 +0000

    INCIDEN-660: Update SSMClient to have a 30s timeout

--- lambdas/get-addresses/src/lib/param-store-client.ts
+++ lambdas/get-addresses/src/lib/param-store-client.ts
@@ -1,3 +1,17 @@
 import { SSMClient } from "@aws-sdk/client-ssm";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
+import { Agent } from "https";
 
-export const SsmClient = new SSMClient({ region: "eu-west-2" });
+const DEFAULT_CONNECTION_TIMEOUT = 30000;
+const DEFAULT_SOCKET_TIMEOUT = 30000;
+
+const requestHandler = new NodeHttpHandler({
+    httpsAgent: new Agent(),
+    connectionTimeout: DEFAULT_CONNECTION_TIMEOUT,
+    socketTimeout: DEFAULT_SOCKET_TIMEOUT,
+});
+
+export const SsmClient = new SSMClient({
+    region: "eu-west-2",
+    requestHandler,
+});
