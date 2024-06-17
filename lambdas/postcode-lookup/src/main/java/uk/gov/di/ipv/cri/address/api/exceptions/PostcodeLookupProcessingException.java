ec05de205ff9074d2b2bb8784140aa2c31b3f25a
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-06-17 09:05:38 +0100

    OJ-2525: modified PostcodeLookupProcessingException

--- lambdas/postcode-lookup/src/main/java/uk/gov/di/ipv/cri/address/api/exceptions/PostcodeLookupProcessingException.java
+++ lambdas/postcode-lookup/src/main/java/uk/gov/di/ipv/cri/address/api/exceptions/PostcodeLookupProcessingException.java
@@ -1,19 +1,11 @@
 package uk.gov.di.ipv.cri.address.api.exceptions;
 
 public class PostcodeLookupProcessingException extends RuntimeException {
-    public PostcodeLookupProcessingException() {
-        super();
-    }
-
     public PostcodeLookupProcessingException(String message) {
         super(message);
     }
 
     public PostcodeLookupProcessingException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public PostcodeLookupProcessingException(Throwable cause) {
-        super(cause);
     }
 }
