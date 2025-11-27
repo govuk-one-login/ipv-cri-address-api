dde2e6fac3b15e53d529fe83209fad3a8986b576
Suraj Kumar <sk96.uk@gmail.com>
2025-11-27 14:52:32 +0000

    test: clean

--- lambdas/postcode-lookup/src/main/java/uk/gov/di/ipv/cri/address/api/Clean.java
+++ lambdas/postcode-lookup/src/main/java/uk/gov/di/ipv/cri/address/api/Clean.java
@@ -2,7 +2,7 @@ package uk.gov.di.ipv.cri.address.api;
 
 public class Clean {
     public static String clean(String metricValue) {
-        if (metricValue == null || metricValue.isEmpty()) {
+        if (metricValue == null || metricValue.isBlank()) {
             return "no_content";
         }
         return metricValue.replace(" ", "_").trim();
